17af0242172da34e63e964d33aeadcb3c362673c
Washington <washingtonfcds@gmail.com>
2022-06-07 12:05:47 -0300

    primeiro commit

---
+++ WebApplication1/Data/WebApiContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Entidades;
+
+namespace WebApplication1.Data
+{
+    public class WebApiContext : DbContext
+    {
+        public WebApiContext(DbContextOptions<WebApiContext > options) : base(options)
+        {
+                
+        }
+        public DbSet <Produto> Produtos { get; set; }
+    }
+}
